b6e89f914fe69203e8d1894c70a96130ebedb325
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-03-23 15:17:54 +0000

    Invoke program setup functions

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
 #include <Windows.h>
+#include "core/platform/program.h"
 
 int __stdcall WinMain(
     HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
+    xbpInitProgram();
+    xbpSaveArguments(hInstance, hPrevInstance, lpCmdLine, nShowCmd);
+    xbpFreeProgram();
 
+    return 0;
 }
